8eb63d47b3e6d357009e2be0e6e27c0306988408
Igor de Almeida <igor.almeida@tron.com.br>
2015-04-13 18:06:27 -0300

    while statement; code structure

--- bcomp.py
+++ bcomp.py
@@ -11,7 +11,7 @@ class Code(object):
     global_vars = []
     functions = []
 
-    last_function = None
+    current_block = None
 
     def add_function(self, x):
         print x
@@ -27,7 +27,7 @@ class Parser(object):
         rpar = Literal(')').suppress()
         plusop = oneOf('+ -')
         multop = oneOf('/ * %')
-        logical_binary_operators = oneOf('& |')
+        logical_binary_operators = oneOf('& | ^')
         inc_op = Word('+', exact=2) | Word('-', exact=2)
 
         expression = Forward()
@@ -38,27 +38,36 @@ class Parser(object):
         function_call = ((identifier('name') + FollowedBy('(')) + lpar + Optional(arguments)('args') + rpar)
 
         precendence = [
-            (inc_op, 1, opAssoc.RIGHT),
-            (inc_op, 1, opAssoc.LEFT),
             (multop, 2, opAssoc.LEFT),
             (plusop, 2, opAssoc.LEFT),
             (logical_binary_operators, 2, opAssoc.LEFT)
         ]
-        expression << operatorPrecedence(function_call | constant | identifier, precendence).setParseAction(self.expression)
+        expression << (operatorPrecedence(function_call | constant | identifier, precendence))
         logical_expression = operatorPrecedence(expression,
                                                 [(logical_binary_operators, 2, opAssoc.LEFT)])
-        assignment = identifier('var') + Word('=') + expression('value');
+        assignment = (identifier + Word('=') + expression('value')).setParseAction(self.assignment)
 
         # statements
         return_statement = Keyword('return') + expression + Suppress(';')
         external_statement = Keyword('extrn') + identifier + Suppress(';')
-        declaration_statement = Keyword('auto') + identifier + Suppress(';')
+
+        auto_atom = identifier + Optional(constant)
+        declaration_statement = Keyword('auto') + auto_atom + ZeroOrMore(
+            Literal(',') + auto_atom) + Suppress(';')
         assignment_statement = assignment + Suppress(';')
         function_call_statement = function_call + Suppress(';')
 
-        if_statement = Keyword('if') + Suppress('(') + (assignment | expression) + Suppress(')') + (
-            statement | (Suppress('{') + statement_list + Suppress('}'))) + Optional(
-            Keyword('else') + (statement | (Suppress('{') + statement_list + Suppress('}'))))
+        if_statement = Keyword('if').setParseAction(self.begin_if) + (
+            Suppress('(') + (assignment | expression) + Suppress(')')) + Empty().setParseAction(self.if_block) + (
+            statement | (Suppress('{') + statement_list + Suppress('}'))) + Empty().setParseAction(
+            self.finish_block) + Optional(
+            Keyword('else') + (statement | (Suppress('{') + statement_list + Suppress('}')))) \
+                           .setParseAction(self.finish_block)
+
+        while_statement = Keyword('while').setParseAction(self.begin_while) + Suppress('(') + (
+            assignment | expression) + Suppress(')') + Empty().setParseAction(self.while_block) + (
+                              statement | Suppress('{') + statement_list + Suppress('}')) + Empty().setParseAction(
+            self.finish_block)
 
         statement << (
             return_statement |
@@ -66,21 +75,46 @@ class Parser(object):
             declaration_statement |
             assignment_statement |
             function_call_statement |
-            if_statement
+            if_statement |
+            while_statement
         )
         statement_list << OneOrMore(statement)
 
         parameter_list = delimitedList(identifier)
         function_body = Suppress('{') + statement_list + Suppress('}')
-        function = identifier('name') + Group(
-            Suppress('(') + Optional(parameter_list)('params') + Suppress(')')) + function_body
+        function = Empty().setParseAction(self.begin_function) + (identifier('name') + Group(
+            Suppress('(') + Optional(parameter_list)('params') + Suppress(
+                ')'))) + function_body + Empty().setParseAction(self.finish_block)
 
         global_declaration = identifier + constant + Suppress(';')
 
         self.program = OneOrMore(function | global_declaration)
 
-    def expression(self, value):
-        print value
+        print
+
+    def assignment(self, value):
+        pass
+
+    def begin_if(self, value):
+        pass
+
+    def if_block(self, value):
+        pass
+
+    def begin_while(self, value):
+        pass
+
+    def while_block(self, value):
+        pass
+
+    def else_block(self, value):
+        pass
+
+    def begin_function(self, value):
+        pass
+
+    def finish_block(self, value):
+        pass
 
     def parse_string(self, string):
         return self.program.ignore(cStyleComment).parseString(string)
@@ -97,11 +131,16 @@ if __name__ == '__main__':
 
     printn(n,b) {
         extrn putchar;
-        auto a;
+        auto a 12, b;
 
-        if(a=n/b) /* assignment, not test for equality */
+        if(a=n/b) { /* assignment, not test for equality */
             printn(a, b); /* recursive */
+        }
         putchar(n%b + '0');
+
+        while(a) {
+            a = b;
+        }
 }
     """
 
